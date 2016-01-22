f4488a71fdc137cc69df094c7c87e879bac50dd6
Kalindi- <kalindi.fonda@gmail.com>
2016-01-21 23:21:17 -0400

    favicon, markers,little fixes

--- js/zzjj.js
+++ js/zzjj.js
@@ -1,10 +1,19 @@
-      function initialize() {
-        var mapCanvas = document.getElementById('map');
-        var mapOptions = {
-          center: new google.maps.LatLng(45.5403, 13.5463),
-          zoom: 10,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
-        var map = new google.maps.Map(mapCanvas, mapOptions)
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
+function initialize() {
+  var mapCanvas = document.getElementById('map');
+  var myLatLng = {lat: 45.530073, lng: 13.651940};
+  var mapOptions = {
+    center: new google.maps.LatLng(myLatLng),
+    zoom: 13,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(mapCanvas, mapOptions)
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
+  });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+
+

9ef37a62d4574d1637f6bb117d4231eb7da88bbc
Kalindi Fonda <kalindi.fonda@gmail.com>
2018-02-13 15:30:26 +0100

    fix: change form address

--- zzjj.js
+++ zzjj.js
@@ -11,7 +11,7 @@ function initialize() {
   var marker = new google.maps.Marker({
     position: myLatLng,
     map: map,
-    title: 'Hello World!'
+    title: 'ZZJJ'
   });
 }
 google.maps.event.addDomListener(window, 'load', initialize);
